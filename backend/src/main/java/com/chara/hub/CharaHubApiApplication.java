08d3c40fc47fe822df3e0d125f4486f44a09692b
JIRAWAT <jirawatsong99@gmail.com>
2026-05-10 02:15:10 +0700

    Scaffold Spring Boot backend

---
+++ backend/src/main/java/com/chara/hub/CharaHubApiApplication.java
@@ -0,0 +1,13 @@
+package com.chara.hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharaHubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharaHubApiApplication.class, args);
+	}
+
+}
